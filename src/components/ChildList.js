c29d3b1abd4776381926cf33d94e5843df7cb382
Sunil Karki <karkisunil1200@gmail.com>
2020-02-06 11:26:27 -0600

    Child renderin

--- src/components/ChildList.js
+++ src/components/ChildList.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
+import React, {useState, useEffect} from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 
-import deleteChild from "../action/deleteChild";
+import deleteChild from '../action/deleteChild';
 
-import getChildren from "../action/getChildren";
+import getChildren from '../action/getChildren';
+import Child from './Child';
 
 const ChildList = props => {
   useEffect(() => {
@@ -23,12 +24,10 @@ const ChildList = props => {
     <div>
       <h3>These are your kids </h3>
       {props.childrenList.map(item => {
-        console.log("Here i am", item);
+        // console.log("Here i am", item);
         return (
           <div>
-            <h3 key={item.username}>
-              Name: <Link to={`./childrenlist/${item.id}`}>{item.fstname}</Link>
-            </h3>
+            <Child item={item} />
             <button onClick={() => deleteChild(item.id)}>delete</button>
           </div>
         );
@@ -37,13 +36,16 @@ const ChildList = props => {
   );
 };
 
-const mapStateToProps = ({ getChildrenReducer }) => {
-  console.log("MapState: ", getChildrenReducer.children);
+const mapStateToProps = ({getChildrenReducer}) => {
+  console.log('MapState: ', getChildrenReducer.children);
   return {
     childrenList: getChildrenReducer.children
   };
 };
 
-export default connect(mapStateToProps, { getChildren, deleteChild })(
-  ChildList
-);
+export default connect(
+  mapStateToProps,
+  {getChildren, deleteChild}
+)(ChildList);
+
+// <Link to={`./childrenlist/${item.id}`}>{item.fstname}</Link>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,13 @@
-import React from "react";
-import TodoList from "./TodoList";
+import React from 'react';
+import TodoList from './TodoList';
 
-import ChildList from "./ChildList";
+import ChildList from './ChildList';
 const Home = () => {
   return (
     <div>
       <h4>Welcome to your Home</h4>
       <ChildList />
-      <TodoList />
+      {/* <TodoList /> */}
     </div>
   );
 };
